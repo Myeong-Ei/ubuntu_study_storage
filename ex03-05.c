b96b23f0e03910b181e7cf26a74310a834b1495f
myeongseok-dsm1101 <tametabby1@naver.com>
2023-03-23 20:30:18 -0700

    2023-3-24

---
+++ ex03-05.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int filedes;
+	off_t newpos;
+	
+	filedes = open(argv[1], O_RDONLY);
+	
+	newpos = lseek(filedes, (off_t)3, SEEK_SET);
+//	newpos = lseek(filedes, (off_t)0, SEEK_END);
+
+	printf("file size : %ld\n", newpos);
+}
