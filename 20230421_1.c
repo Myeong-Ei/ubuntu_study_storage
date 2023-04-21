7d264961764be01cda3795d6b90715a52d06e5c6
myeongseok-dsm1101 <tametabby1@naver.com>
2023-04-20 18:49:29 -0700

    2023-04-21

---
+++ 20230421_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+	const char *str = "Sorry, Mr.Kang. I don't know what should I do this. Could you let me know hot to solve it?";
+	printf("%s\n", str); 
+}
